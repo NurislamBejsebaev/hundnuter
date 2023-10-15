6becd3d4a7e748b73768c281a810c0b9762e0bc2
NurislamBejsebaev <130899878+NurislamBejsebaev@users.noreply.github.com>
2023-10-16 00:36:08 +0600

    'a-litle-changeincode'

--- core/views.py
+++ core/views.py
@@ -104,7 +104,7 @@ def reg_view(request):
             username=request.POST["username"]
         )
         user.set_password(request.POST["password"])
-        return redirect('home')
+        return redirect('')
 
     return render(
         request,
@@ -186,3 +186,4 @@ def company_edit(request, id):
             return redirect(company_info, id=company.id)
         else:
             return HttpResponse('ajhvf yt dfkblyf')
+
